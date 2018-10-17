120769480653afa46419c8cb3ecf1ec058eb4744
justin <justin.martin.developper@gmail.com>
2018-10-17 17:56:55 +0200

    init commit

---
+++ server.js
@@ -0,0 +1,53 @@
+// BASE SETUP
+// =============================================================================
+
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+var request = require('request');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = 3001;
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    //todo logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'welcome to MyAgenda-API!' });
+});
+// more routes for our API will happen here
+
+router.route('/redirectURL/')
+    .get(function(req, res) {
+        var url = req.params.url;
+
+        request("https://"+url, function (error, response, body) {
+            if(response && response.statusCode) {
+                if (error)
+                    res.json({Error: error + " (code: " + response.statusCode + ")"});
+                res.json({message: body});
+            }
+        });
+    });
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
